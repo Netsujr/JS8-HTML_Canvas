b466268cbbe2bc2e273f57c1aeec727875fe8a91
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-21 21:58:26 +0900

    canvas works, need to add easter egg

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+// ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(event) {
   if(!isDrawing) return; //if not drawing, dont log mouse movement
   console.log(event);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   //start from here
   ctx.moveTo(lastX, lastY);
@@ -22,6 +26,18 @@ function draw(event) {
   // lastX = event.offsetX;
   // lastY = event.offsetY;
   [lastX, lastY] = [event.offsetX, event.offsetY];
+  hue++;
+  if(hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 30 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if(direction){
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 canvas.addEventListener('mousedown', (event) => {
